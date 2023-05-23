362367af5399f9e673cbd9e2a07fa557960389b8
rex <yu.chenhang@gmail.com>
2023-05-23 14:12:50 +0800

    使用 https

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,5 +1,6 @@
 const enc = new TextEncoder();
-const dec = new TextDecoder();
+
+// const dec = new TextDecoder();
 
 function login() {
     $.ajax({
@@ -9,7 +10,6 @@ function login() {
         dataType: 'json',
         data: JSON.stringify({username: $('#username').val()}),
         success: function (data) {
-            console.info(data);
             navigator.credentials.get({publicKey: convertArrayBuffer(data)})
                 .then(newCredentialInfo => authenticate(newCredentialInfo))
                 .catch(error => console.error(error));
@@ -28,8 +28,7 @@ function preregister() {
             displayName: $('#displayName').val()
         }),
         success: function (response) {
-            console.info(response);
-            if(response.code === '200') {
+            if (response.code === '200') {
                 navigator.credentials.create({publicKey: convertArrayBuffer(response.data)})
                     .then(newCredentialInfo => register(newCredentialInfo, response.data.user.id))
                     .catch(error => console.error(error));
@@ -46,9 +45,8 @@ function convertArrayBuffer(publicKeyCredentialCreationOptions) {
         publicKeyCredentialCreationOptions['user']['id'] = enc.encode(publicKeyCredentialCreationOptions['user']['id']);
     }
     // FIXME 不刪除這個屬性無法在 wisecure-tech.com 以外的地方測試
-    if (publicKeyCredentialCreationOptions['rp']) {
-        delete publicKeyCredentialCreationOptions['rp']['id'];
-    }
+    // if (publicKeyCredentialCreationOptions['rp']) {
+    //     delete publicKeyCredentialCreationOptions['rp']['id'];
     return publicKeyCredentialCreationOptions;
 }
 
@@ -66,7 +64,7 @@ function register(credentialInfo, userId) {
             },
             type: credentialInfo.type,
             username: $('#username').val(),
-            userId: dec.decode(userId)
+            userId: uint8ArrayToString(userId)
         }),
         success: data => console.info(data),
         error: error => console.error(error)
@@ -76,6 +74,48 @@ function register(credentialInfo, userId) {
 function authenticate(credential) {
     // FIXME 還沒做
     console.info(credential);
+}
+
+function uint8ArrayToString(array) {
+    let out = "";
+    let i = 0;
+    let len = array.length;
+    let c;
+    let char2
+    let char3;
+
+    while (i < len) {
+        c = array[i++];
+        switch (c >> 4) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                // 0xxxxxxx
+                out += String.fromCharCode(c);
+                break;
+            case 12:
+            case 13:
+                // 110x xxxx   10xx xxxx
+                char2 = array[i++];
+                out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+                break;
+            case 14:
+                // 1110 xxxx  10xx xxxx  10xx xxxx
+                char2 = array[i++];
+                char3 = array[i++];
+                out += String.fromCharCode(((c & 0x0F) << 12) |
+                    ((char2 & 0x3F) << 6) |
+                    ((char3 & 0x3F) << 0));
+                break;
+        }
+    }
+
+    return out;
 }
 
 function arrayBufferToBase64(arrayBuffer) {
